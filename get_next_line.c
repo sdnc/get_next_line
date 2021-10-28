e499cf06cd4d7eeec3c8235ff3e7f92d36fcd034
sdnc <dza.sara@gmail.com>
2021-10-28 22:44:13 +1030

    281021

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,65 @@
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char    *append(int fd, char *buf, char *remainder)
 {
+    int     lines_read;
+    char    *temp;
 
+    lines_read = 1;
+    while (lines_read != 0)
+    {
+        lines_read = read(1, buf, BUFFER_SIZE);
+        if (lines_read == -1) // This means that there's an error
+            return (NULL);
+        else if (lines_read == 0) // This means EOF
+            break ;
+        buf[lines_read] = '\0';
+        if (!remainder)
+            return (ft_strdup(""));
+        temp = remainder;
+        remainder = ft_strjoin(temp, buf);
+        free(temp);
+        if (ft_strchr (buf, '\n'))
+            break ;
+    }
+    return (remainder);
+}
+
+static char *extract(char *line)
+{
+    size_t  i;
+    char    *remainder;
+
+    i = 0;
+    while (line[i] != '\n' && line[i] != '\0')
+        i++;
+    if (line[i] == '\0' || line[1] == '\0')
+        return (0);
+    remainder = ft_substr(line, i + 1, ft_strlen(line) - 1);
+    if (!remainder)
+    {
+        free(remainder);
+        remainder = NULL;
+    }
+    line[i + 1] = '\0';
+    return (remainder);
+}
+
+char	*ft_get_next_line(int fd)
+{
+    static char *remainder;
+    char        *buf;
+    char        *line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buf)
+    return (NULL);
+    line = append(fd, buf, remainder);
+    free(buf);
+    if (!line)
+        return (NULL);
+    remainder = extract(line);
+    return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,5 +3,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# define BUFFER_SIZE 1042
 
 #endif
